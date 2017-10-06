27875481716a25b3fd13dc51b9786e0a9f9149bf
Roy Schwartz <roys@roy-dev.dev.ai2>
2017-10-06 21:08:33 +0000

    Added a few command line options, not printing sample embeddings

--- scripts/run_docker_dsrl.sh
+++ scripts/run_docker_dsrl.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+o_dir=/output/
+o_file=$o_dir/metrics.json
+
 v=`echo "$MIN_THR < $MAX_THR" | bc`;
 if [ $v -eq 1 ]; then
     echo "$MIN_THR < $MAX_THR"
-    echo "{" > /output/metrics.json
+    echo "{" > $o_file
     for i in A0 A1 A2 A3; do
-        echo "\"$i\": -1," >> /output/metrics.json
+        echo "\"$i\": -1," >> $o_file
     done
 
-    echo "\"Overall\": -1" >> /output/metrics.json
-    echo "}" >> /output/metrics.json
+    echo "\"Overall\": -1" >> $o_file
+    echo "}" >> $o_file
     exit 0
 #else
 #    echo "$MIN_THR >= $MAX_THR ($v)! great"
@@ -18,7 +21,7 @@ fi
 emb_file=${PREFIX}_${MIN_THR}_${MAX_THR}.dat.gz
 
 scripts/run_predict_conll05.sh ${TEST_FILE} \
-                ${MODEL_DIR} "--embeddings $emb_file $ADDITIONAL_PARAMS" | tee out.log
+                ${MODEL_DIR} "--embeddings $emb_file -d $o_dir $ADDITIONAL_PARAMS" | tee out.log
 
 ret_val=$?
 echo $out
@@ -31,18 +34,18 @@ function write_val {
 
     val=`grep " ${w} " out.log | awk '{print $7}'`
 
-    echo "  \"${w}\": ${val}$suf" >> /output/metrics.json
+    echo "  \"${w}\": ${val}$suf" >> $o_file
 
 
 }
 if [ $ret_val -eq 0 ]; then
-    echo "{" > /output/metrics.json
+    echo "{" > $o_file
     for i in A0 A1 A2 A3 AM-LOC AM-TMP; do
         write_val $i ","
     done
 
     write_val "Overall" ""
-    echo "}" >> /output/metrics.json
+    echo "}" >> $o_file
 fi
 
 /bin/rm out.log
